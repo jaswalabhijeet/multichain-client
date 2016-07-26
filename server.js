18522c1802342ffdce3fdba3c19a906a507c2920
vksah32 <vksah@colby.edu>
2016-07-26 13:37:15 -0400

    basic functions work

---
+++ server.js
@@ -0,0 +1,81 @@
+var express = require('express'),
+  app = express(),
+  http = require('http'),
+  bodyParser = require('body-parser'),
+  httpServer = http.Server(app);
+
+var multichain = require("multichain-node")({
+    port: 6268,
+    host: 'localhost',
+    user: "multichainrpc",
+    pass: "BMeUFamf4fu4dnDL7msryBaVzDXqNuu16iQEuj1grrpm"
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res) {
+  res.sendfile(__dirname + '/index.html');
+});
+app.get('/balance', function(req,res){
+	var k = 0;
+	multichain.getTotalBalances( function (err, balance) {
+      	if (err) {
+           console.log(err);
+           //res.status(400);
+	         res.send(err);
+       } else  {
+           console.log('Hello Money! New balance: ' + balance[0]['qty']);
+
+	         res.header('Content-type', 'text/html');
+ 	          var html = 'The new balance is ' + balance[0]['qty'] + '';
+           res.end(html);    
+              
+	}
+   });
+});
+
+/*
+route for send asset function
+ */
+app.post('/send',function(req,res){
+	var qty = req.body.qty;
+  var someAsset = req.body.asset;
+	var someAddress = req.body.address;
+
+	multichain.sendAssetToAddress({address:someAddress, asset: someAsset, qty:parseInt(qty)},(err,data) =>{
+    if(err){
+        res.status(400).send(err);
+        // console.log(err);
+        // res.send(err)
+    } else{
+      console.log(data);
+      res.send(data)
+    }
+
+    
+  });
+});
+
+app.get('/addresses',function(req,res){
+  multichain.getAddresses((err,data) =>{
+    if(err){
+        console.log(err);
+        res.status(400);
+        res.send(err);
+    } else{
+      console.log(data);
+      res.header('Content-type', 'text/html');
+      // var html = '<h4>' + data[0]+ '</h4>';
+      return res.end(data + "");
+    }
+
+    
+  });
+});
+
+
+app.listen(3000);
